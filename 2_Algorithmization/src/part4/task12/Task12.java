63ee1370b06037c2b72ec0311dddcecdbacadacd
Gradoma <gradomski25@gmail.com>
2019-12-01 13:01:11 +0300

    part 4, task 12 corrected

--- 2_Algorithmization/src/part4/task12/Task12.java
+++ 2_Algorithmization/src/part4/task12/Task12.java
@@ -7,49 +7,48 @@ import java.util.Arrays;
 
 public class Task12 {
     public static void main(String[] args) {
-        int k = 13;
-        int n = 157;
+        int k = 14;
+        int n = 57;
         int [] a;
 
-        int i=1;
-        int counter=0;
-        while (i <= n){
-            if (findSumOfNumerals(i, findLength(i)) == k){
-                System.out.println(i + " - num");
-                counter++;
-            }
-            i++;
-        }
-        a = new int[counter];
-        if (counter == 0){
-            System.out.println("No numbers");
+        System.out.println("Result : " + Arrays.toString(findNumbersToArray(k, n)));
+    }
+
+    private static int [] findNumbersToArray (int k, int n) {
+        int size = findAmount(k,n);
+        int [] array = new int[size];
+        if (size == 0){
+            return array ;
         } else {
-            i = 1;
-            for (int j=0; j<counter; j++){
+            int i = 1;
+            for (int j=0; j<size; j++){
                 while (i <= n){
-                    if (findSumOfNumerals(i, findLength(i)) == k){
-                        a[j] = i;
+                    if (findSumOfNumerals(i) == k){
+                        array[j] = i;
                         i++;
                         break;
                     }
                     i++;
                 }
             }
-
         }
-        System.out.println(Arrays.toString(a));
+        return array;
     }
 
-    private static int findLength (int n){
-        int length = 1;
-        while (n/10 != 0){
-            length++;
-            n = n/10;
+    private static int findAmount (int k, int n){
+        int i=1;
+        int counter=0;
+        while (i <= n){
+            if (findSumOfNumerals(i) == k){
+                counter++;
+            }
+            i++;
         }
-        return length;
+        return counter;
     }
 
-    private static int findSumOfNumerals (int n, int length){
+    private static int findSumOfNumerals (int n){
+        int length = findAmountOfNumerals(n);
         int [] array = new int[length];
         if (length == 1){
             return n;
@@ -63,5 +62,14 @@ public class Task12 {
             sum += i;
         }
         return sum;
+    }
+
+    private static int findAmountOfNumerals(int n){
+        int length = 1;
+        while (n/10 != 0){
+            length++;
+            n = n/10;
+        }
+        return length;
     }
 }
