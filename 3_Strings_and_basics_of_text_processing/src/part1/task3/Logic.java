cf8914fd565d9ab72730e6eb4313f0ff8af09255
Gradoma <gradomski25@gmail.com>
2019-12-03 19:32:36 +0300

    part 1, task 3 completed

---
+++ 3_Strings_and_basics_of_text_processing/src/part1/task3/Logic.java
@@ -0,0 +1,14 @@
+package part1.task3;
+
+public class Logic {
+    int countAllNumerals (String str){
+        int counter = 0;
+        char[] strChar = str.toCharArray();
+        for (int i=0; i<str.length(); i++){
+            if ((int)strChar[i] >= 48 & (int)strChar[i] <= 57 ){
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ 3_Strings_and_basics_of_text_processing/src/part1/task3/Task3.java
@@ -0,0 +1,13 @@
+// В строке найти количество цифр.
+
+package part1.task3;
+
+public class Task3 {
+    private String example = "1 hour contain 3600 seconds.";
+
+    public static void main(String[] args) {
+        Task3 task3 = new Task3();
+        Logic logic = new Logic();
+        System.out.println("Numerals in string: " + logic.countAllNumerals(task3.example));
+    }
+}
